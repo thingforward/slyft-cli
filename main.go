265181807d749c417f855d7393003274eb42cee3
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2016-11-27 14:03:18 +0100

    setup logging and backend server supply via env

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
+	"github.com/op/go-logging"
 	"github.com/urfave/cli"
 )
 
@@ -12,6 +14,33 @@ import (
 // The *preferred* way can be seen here: https://github.com/urfave/cli#flags
 var ProjectName string
 var Everything bool
+var BackendBaseUrl = os.Getenv("SLYFTBACKEND")
+
+var log = logging.MustGetLogger("ibtlogger")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func setupLogger() {
+	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
+	logBackendWithFormat := logging.NewBackendFormatter(logBackend, format)
+	loggerLeveled := logging.AddModuleLevel(logBackendWithFormat)
+
+	level, err := logging.LogLevel(os.Getenv("DEBUGLEVEL"))
+	if err != nil {
+		fmt.Println("Logging level is set to %s", level)
+	}
+
+	loggerLeveled.SetLevel(level, "")
+	logging.SetBackend(loggerLeveled)
+}
+
+func init() {
+	setupLogger()
+	if BackendBaseUrl == "" {
+		log.Fatal("Backend URL missing, please contact tech support")
+	}
+}
 
 func main() {
 	app := cli.NewApp()
